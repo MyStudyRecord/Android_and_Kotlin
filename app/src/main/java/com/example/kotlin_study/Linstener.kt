aad5599c6a534a12332a0f894e988c9e5d395f4f
ParkSangSun1 <appleasz1028102@gmail.com>
2020-12-14 19:30:26 +0900

    Linstener

---
+++ app/src/main/java/com/example/kotlin_study/Linstener.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.kotlin_study.kotlin.hello
+
+class Linstener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linstener)
+
+        //뷰를 activity로 가져오는 방법
+//        1. 직접 찾아서 가져온다다
+//       val textView : TextView = findViewById(R.id.hello)
+//
+//        2. xml을 import해서 가져온다
+//        hello.
+    }
+}
