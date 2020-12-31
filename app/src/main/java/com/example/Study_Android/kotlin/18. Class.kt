ab882b2d20fbb2b1321fed8bf6f1d7559c3aebc1
ParkSangSun1 <appleasz1028102@gmail.com>
2020-12-31 21:36:00 +0900

    class 오버로딩, 인스턴스 만드는법, 멤버변수 접근, init, 메소드

--- app/src/main/java/com/example/Study_Android/kotlin/18. Class.kt
+++ app/src/main/java/com/example/Study_Android/kotlin/18. Class.kt
@@ -32,6 +32,23 @@ fun main(array: Array<String>){
 
 
     val superCar : SuperCar=SuperCar("good engine","big","white")
+
+    val runableCar: RunableCar = RunableCar("simple engine","shot body")
+    runableCar.ride()
+    runableCar.navi("서울")
+    runableCar.drive()
+
+    //인스턴스의 멤버 변수에 접근 하는 방법
+    val runableCar2 : RunableCar2 = RunableCar2("nice","long")
+    println(runableCar2.body)
+    println(runableCar2.engine)
+
+
+    val testCalss = TestCalss()
+    testCalss.test(2,1)
+    testCalss.test(1)
+
+
 }
 
 //클래스(설명서) 만드는 방법(1)
@@ -45,6 +62,7 @@ class SuperCar{
     var body:String
     var door: String
 
+
     constructor(engine: String,body: String, door: String){
         this.engine=engine
         this.body=body
@@ -52,7 +70,78 @@ class SuperCar{
     }
 }
 
-//클래스(설명서) 만드는 방법(3) -> 1번 방법으 확장
+//클래스(설명서) 만드는 방법(3) -> 1번 방법의 확장
 class Car1(engine: String, body: String){
-    //var door :String
+    var door :String=""
+
+    constructor(engine: String,body: String,door: String):this(engine,body){
+        this.door=door
+    }
+}
+
+//클래스(설명서) 만드는 방법(4) -> 2번 방법의 확장
+class Car2{
+    var engine: String =""
+    var body:String=""
+    var door: String=""
+
+    constructor(engine: String,body: String){
+        this.engine=engine
+        this.body=body
+    }
+    constructor(engine: String,body: String,door: String){
+        this.body=body
+        this.door=door
+        this.engine=engine
+    }
+}
+
+
+class RunableCar(engine: String,body: String){
+    fun ride(){
+        println("탑승 하였습니다")
+    }
+    fun drive(){
+        println("달립니다 !")
+    }
+    fun navi(destination : String){
+        println("$destination 으로 목적지가 설정되었습니다")
+    }
+}
+
+class RunableCar2{
+    var engine : String
+    var body : String
+    constructor(engine: String,body: String){
+        this.engine=engine
+        this.body=body
+    }
+
+    //무조건 가장먼저 실행, 무조건 실행
+    //초기셋팅을 할 때 유용하다
+    init{
+        println("RunableCar2가 만들어 졌습니다")
+    }
+
+
+    fun ride(){
+        println("탑승 하였습니다")
+    }
+    fun drive(){
+        println("달립니다 !")
+    }
+    fun navi(destination : String){
+        println("$destination 으로 목적지가 설정되었습니다")
+    }
+
+}
+
+//오버로딩
+class  TestCalss(){
+    fun test(a:Int){
+        println("1")
+    }
+    fun test(a:Int,b:Int){
+        println("2")
+    }
 }
