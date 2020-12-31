3fc9acbc7467d7d97a6d016f5b92cfdd8c3d693e
ParkSangSun1 <appleasz1028102@gmail.com>
2020-12-31 19:26:59 +0900

    파일 이름변경

--- app/src/androidTest/java/com/example/Study_Android/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/Study_Android/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.kotlin_study
+package com.example.Study_Android
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/test/java/com/example/Study_Android/ExampleUnitTest.kt
+++ app/src/test/java/com/example/Study_Android/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.kotlin_study
+package com.example.Study_Android
 
 import org.junit.Test
 
