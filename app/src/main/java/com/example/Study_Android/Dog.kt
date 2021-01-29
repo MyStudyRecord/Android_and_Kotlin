b4f99085034f5cdba842a0bbca52dc65eb4e24ec
ParkSangSun1 <appleasz1028102@gmail.com>
2021-01-29 23:08:45 +0900

    dog예제

---
+++ app/src/main/java/com/example/Study_Android/Dog.kt
@@ -0,0 +1,11 @@
+package com.example.Study_Android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog)
+    }
+}
