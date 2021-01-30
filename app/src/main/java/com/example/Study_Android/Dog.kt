966b4f8f842705b600bface65cff8b1e872cd8c3
ParkSangSun1 <appleasz1028102@gmail.com>
2021-01-30 20:43:17 +0900

    코틀린 문법 복습

--- app/src/main/java/com/example/Study_Android/Dog.kt
+++ app/src/main/java/com/example/Study_Android/Dog.kt
@@ -2,10 +2,23 @@ package com.example.Study_Android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dog.*
 
 class Dog : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog)
+
+        var dog:String
+        var dog2:String
+
+        button3.setOnClickListener {
+            dog = editTextTextPersonName.text.toString()
+            textView.setText(dog)
+        }
+        button4.setOnClickListener {
+            dog2 = editTextTextPersonName2.text.toString()
+            textView.setText(dog2)
+        }
     }
 }
